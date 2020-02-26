b2358d9ce8a9d61c76c3885ad2246d150f3500d9
bobi stonks <58303796+bobkataelittt@users.noreply.github.com>
2020-02-26 18:57:43 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                arr[i] = input;
+            }
+            Array.Reverse(arr);
+            foreach (string val in arr)
+            {
+                Console.Write($" {val} ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            int[] arr3 = new int[n];
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                arr1[i] = input;
+            }
+
+            int[] arr2 = new int[n];
+            arr1.CopyTo(arr2, 0);
+
+            Array.Reverse(arr2);
+
+            for (int i = 0; i < n; i++)
+            {
+                arr3[i] = arr1[i] + arr2[i];
+            }
+            foreach (int val in arr3)
+            {
+                Console.Write($" {val} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
